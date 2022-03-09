b39db059a09cf95c3e5a27fe6d594773cd506bd5
Ofek <ofekel@ac.sce.ac.il>
2022-03-09 15:02:20 +0200

    Fixed Question 4

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ while eps + 1 > 1:
     eps /= 2
 eps *= 2
 print("The machine epsilon is:", eps)
-
-#Q3 + Q4
+#
+# #Q3 + Q4
 ans = abs(3.0*(4.0/3.0-1)-1)
 print("Answer is:", ans)
 
-fixed = abs(round(3.0*(4.0/3.0-1)-1))
+fixed = abs((3.0*(4.0/3.0-1)-1)+eps)
 print("Fixed Answer is:", fixed)
 
 
